5564af1f1bfca56cb0166f38985d94841645c0c9
zakaria-samlali <samlali.zakaria@gmail.com>
2024-03-04 17:54:06 +0100

    add script file for the quiz

--- script.js
+++ script.js
@@ -42,8 +42,10 @@ const quizData = [
     const d_text = document.getElementById("d_text");
     const submitBtn = document.getElementById("submit");
 
-    const currentQuiz = 0;
+    let currentQuiz = 0;
     let score = 0;
+    
+    loadQuiz();
 
     function loadQuiz(){
         deselectAnswers();
@@ -61,8 +63,31 @@ const quizData = [
         });
     }
 
+    function getSelected(){
+        let answer;
+        answerEls.forEach(answerEl => {
+            if(answerEl.checked){
+                answer = answerEl.id;
+            }
+        })
+        return answer;
+    }
+
     submitBtn.addEventListener("click", () => {
         const answer = getSelected();
-
-        
-    });
+        if(answer){
+            if(answer === quizData[currentQuiz].correct){
+                score++;
+            }
+            currentQuiz++;
+            if(currentQuiz < quizData.length){
+                loadQuiz();
+            } else {
+                quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+                `;
+            }
+        }
+    })
