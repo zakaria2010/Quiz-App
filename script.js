7c5a646f8ae66be59ebcbb34e4ae3a7a3f79f75f
zakaria-samlali <samlali.zakaria@gmail.com>
2024-03-04 15:25:55 +0100

    the css code

---
+++ script.js
@@ -0,0 +1,34 @@
+const quizData = [
+        {
+            question: "What is the capital of France?",
+            a: "Paris",
+            b: "London",
+            c: "Berlin",
+            d: "Madrid",
+            correct: "a",
+        },
+        {
+            question: "What is the capital of Spain?",
+            a: "Paris",
+            b: "London",
+            c: "Berlin",
+            d: "Madrid",
+            correct: "d",
+        },
+        {
+            question: "What is the capital of Germany?",
+            a: "Paris",
+            b: "London",
+            c: "Berlin",
+            d: "Madrid",
+            correct: "c",
+        },
+        {
+            question: "What is the capital of England?",
+            a: "Paris",
+            b: "London",
+            c: "Berlin",
+            d: "Madrid",
+            correct: "b",
+        },
+    ];
